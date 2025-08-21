89dd709d0ebbb91174be1c0ff0f1a032e2f115a7
Andreycnz <andreylc42@gmail.com>
2025-08-21 11:59:22 -0300

    fim da aula dia 21/08

--- web_system/aula/models/atividade.py
+++ web_system/aula/models/atividade.py
@@ -3,7 +3,17 @@ from django.db import models
 
 
 class Atividade(Base):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, help_text="Digite o nome do Aluno")
+    preco = models.DecimalField(verbose_name='Preço',
+                                max_digits=10,
+                                decimal_places=2,
+                                default=0.00,
+                                help_text="Digite o valor em reais R$")
+    turno = models.CharField(max_length=8, null=True,
+                             blank=True,
+                             help_text="Digite o turno da aula") #null é para tirar a obrigatoriedade, pode ser usado Blank=True(para front) tambem.
+    nota = models.FloatField(default=0.0, help_text="Digite a nota do aluno")
 
     def __str__(self):
-        return self.nome
+        return f"{self.id} - {self.nome}"
+

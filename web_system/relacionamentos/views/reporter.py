a78bada233fd62bea801892d9c856681b674f265
aluno <andreylc42@gmail.com>
2025-11-12 12:02:28 -0300

    12/11

--- web_system/relacionamentos/views/reporter.py
+++ web_system/relacionamentos/views/reporter.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 import random
 import string
+from django.contrib.auth.decorators import permission_required, login_required
+from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 from django.views import View
 
 from relacionamentos.forms.reporter import ReporterForm
 from relacionamentos.models import Reporter
 
+@require_http_methods(["GET"])
 def reporter_list(request):
     reporter= Reporter.objects.all()
 
@@ -15,6 +18,10 @@ def reporter_list(request):
     }
     return render(request, 'exemplos/reporter.html', contexto)
 
+
+@login_required
+@permission_required("relacionamentos.view_reporter", raise_exception=True)
+@require_http_methods(["GET"])
 def reporter_detail(request, pk):
     reporter = Reporter.objects.get(id=pk)
     contexto = {
@@ -24,6 +31,10 @@ def reporter_detail(request, pk):
 
     return render(request, 'exemplos/reporter_detail.html', contexto)
 
+
+@login_required
+@permission_required("relacionamentos.view_reporter", raise_exception=True)
+@require_http_methods(["GET", "POST"])
 def reporter_delete(request, reporter_id):
     reporter = get_object_or_404(Reporter, pk=reporter_id)
     try:
@@ -44,6 +55,9 @@ def reporter_delete(request, reporter_id):
         print(e)
         return render(request, 'exemplos/reporter.html', contexto)
 
+@login_required
+@permission_required("relacionamentos.view_reporter", raise_exception=True)
+@require_http_methods(["GET"])
 def gerar_codigo(request, reporter_id):
     reporter = get_object_or_404(Reporter, pk=reporter_id)
     try:
@@ -55,6 +69,10 @@ def gerar_codigo(request, reporter_id):
         print(f'Erro ao gerar codigo para o reporter {reporter}')
         return redirect('relacionamentos:reporter_funcao_list')
 
+
+@login_required
+@permission_required("relacionamentos.view_reporter", raise_exception=True)
+@require_http_methods(["GET", "POST"])
 def reporter_create(request):
     if request.method == 'POST':
         form = ReporterForm(request.POST)
@@ -68,6 +86,10 @@ def reporter_create(request):
     }
     return render(request, 'exemplos/create_simple.html', contexto)
 
+
+@login_required
+@permission_required("relacionamentos.view_reporter", raise_exception=True)
+@require_http_methods(["GET", "POST"])
 def reporter_update(request, reporter_id):
     reporter = get_object_or_404(Reporter, pk=reporter_id)
     if request.method == 'POST':
